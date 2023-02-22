6ec6233370c918b628405c45a6e743514530f3c9
ashrxyz <ashtonh@umich.edu>
2023-02-22 17:56:29 -0500

    Updated OSUtils.py
    Input documentation for the class OSUtils and methods B and C.

--- src/thiagodnf/ars/commons/utils/OSUtils.py
+++ src/thiagodnf/ars/commons/utils/OSUtils.py
@@ -1,7 +1,9 @@
 import platform
 
 class OSUtils:
-
+    """! Identifies what operating system 
+    user is on and returns it.
+    """
     @classmethod
     def isWindows(cls):
         """! Verify if the operational system used is Windows
@@ -12,8 +14,14 @@ class OSUtils:
 
     @classmethod
     def isMac(cls):
+        """! Verifies if the operating system used is Mac
+        @return True if it is Mac, False, otherwise.
+        """
         return platform.system() == "Darwin"
 
     @classmethod
     def isUnix(cls):
+        """! Verifies if the operating system used is Linux
+        @return True if it is Linux, False, otherwise.
+        """
         return platform.system() == "Linux"
