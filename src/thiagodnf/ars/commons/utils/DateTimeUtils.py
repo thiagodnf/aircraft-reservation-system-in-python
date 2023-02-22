064c8dd015053185ae936e7054993b71a17339a2
ashrxyz <ashtonh@umich.edu>
2023-02-22 17:47:52 -0500

    Update DateTimeUtils.py
    Had to make changes to documentation

--- src/thiagodnf/ars/commons/utils/DateTimeUtils.py
+++ src/thiagodnf/ars/commons/utils/DateTimeUtils.py
@@ -6,9 +6,10 @@ class DateTimeUtils(object):
     """
     @classmethod
     def getNow(cls):
-        """! this function returns the date and 
-        time of current.
+        """! this function clears the screen 
+        and returns the date and time of current.
         @param cls Clears screen
+        @return date and time currently
         """
         return datetime.datetime.now()
 
@@ -18,6 +19,7 @@ class DateTimeUtils(object):
         time, while putting it into a string.
         @param cls Clears screen
         @param dateTime recieves dateTime info
+        @return date and time in the format given
         """
         format = "%Y-%m-%d %H:%M:%S"
 
