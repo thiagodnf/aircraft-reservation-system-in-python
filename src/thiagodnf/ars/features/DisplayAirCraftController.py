00feb631cf34a0ccc5e99527ca2bc422563b6aab
Christopher Williams <112411677+cwilliams4950@users.noreply.github.com>
2023-02-22 18:02:59 -0500

    Update DisplayAirCraftController.py
    Documented display aircraft controller

--- src/thiagodnf/ars/features/DisplayAirCraftController.py
+++ src/thiagodnf/ars/features/DisplayAirCraftController.py
@@ -4,8 +4,14 @@ from thiagodnf.ars.commons.utils.ConsoleUtils import ConsoleUtils
 from thiagodnf.ars.features.Controller import Controller
 
 class DisplayAirCraftController(Controller):
+    """! This class displays the aircraft seats and shows which ones are reserved and which ones are still available.
+    The user can return to the main menu when finished reviewing the seats.
+    """
 
     def display(self):
+        """! This method displays the aircraft. It prints the aircraft to the screen
+        @return ControllerID.MENU   returns the user to the main menu
+        """
 
         ConsoleUtils.println("Home >> Display Aircraft")
         ConsoleUtils.printLine()
