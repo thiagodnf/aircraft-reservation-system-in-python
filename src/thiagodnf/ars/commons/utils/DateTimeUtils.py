0b276d3764ac895ab4412e26f81fcf10efb45ce6
ashrxyz <ashtonh@umich.edu>
2023-02-22 17:44:33 -0500

    Updated DateTimeUtils.py
    Input documentation for the class DateTimeUtils and the two methods below it

--- src/thiagodnf/ars/commons/utils/DateTimeUtils.py
+++ src/thiagodnf/ars/commons/utils/DateTimeUtils.py
@@ -1,14 +1,24 @@
 import datetime
 
 class DateTimeUtils(object):
-
+    """! deals with getting the date and time 
+    on request and formats it.
+    """
     @classmethod
     def getNow(cls):
+        """! this function returns the date and 
+        time of current.
+        @param cls Clears screen
+        """
         return datetime.datetime.now()
 
     @classmethod
     def format(cls, dateTime):
-
+        """! deals with formatting the date and 
+        time, while putting it into a string.
+        @param cls Clears screen
+        @param dateTime recieves dateTime info
+        """
         format = "%Y-%m-%d %H:%M:%S"
 
         return dateTime.strftime(format)
