8a0854a312c98b5f2350a4e78e7dea74d042e683
Christopher Williams <112411677+cwilliams4950@users.noreply.github.com>
2023-02-22 17:37:48 -0500

    Update ReserveASeatController.py
    Finished adding documentation for the "reserveaseatcontroller" file.

--- src/thiagodnf/ars/features/ReserveASeatController.py
+++ src/thiagodnf/ars/features/ReserveASeatController.py
@@ -9,14 +9,22 @@ class ReserveASeatController(Controller):
 
     def display(self):
 
+        """! This class is used to reserve a seat. It begins by displaying the aircraft in the terminal, then asks the user what seat they want.
+        After selecting a seat, the program prompts for the name.
+        """
+
         ConsoleUtils.println("Home >> Reserve Seat")
         ConsoleUtils.printLine()
 
+        ## Calls the aircraft class and displays the seats on the plane
         Aircraft.print()
 
+        ## Calls the askSeatNumber method
         seatNumber = self.askSeatNumber()
+        ## Calls the askPassengerName method
         passengerName = self.askPassengerName()
 
+        ## Reserves the seat using data from the askPassengerName and seatNumber methods
         Database.getInstance().reserve(seatNumber, passengerName)
 
         return ControllerId.MENU
@@ -27,16 +35,26 @@ class ReserveASeatController(Controller):
         @return the passenger name
         """
 
+        ## Asks for the passenger name
         passengerName = ConsoleUtils.askString("Passenger Name: ")
 
+        ## Exception handling for a blank name
         if StringUtils.isBlank(passengerName):
             raise RuntimeError("The passenger name should not be empty")
 
         return passengerName
 
     def askSeatNumber(self):
+        """"! Asks for the seat number that the passenger wants. If the user does not supply an input, an error is thrown.
+        If an invalid input is entered, an error is thrown. If the wrong format is entered, an error is thrown. If a seat is already reserved, an error is thrown.
+        Each time an error occurs, the user gets to re-enter the information until a valid entry is entered.
+        This program also converts the entered seat number to uppercase.
+        @return seatNumber     This is the seat number that is entered into the database
+        """
 
         seatNumber = ConsoleUtils.askString("Seat Number: ")
+
+        ## Exception handling if statements
 
         if StringUtils.isBlank(seatNumber):
             raise RuntimeError("The seat number should not be empty")
