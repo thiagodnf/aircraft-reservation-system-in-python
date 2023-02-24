a3e40dd78ae022528a0d89c287424bf8a43a0aec
marwahamm04 <112507192+marwahamm04@users.noreply.github.com>
2023-02-24 00:13:58 -0500

    commit changes to class
    added documentation to class

--- src/thiagodnf/ars/commons/Reservation.py
+++ src/thiagodnf/ars/commons/Reservation.py
@@ -1,9 +1,18 @@
 from thiagodnf.ars.commons.utils.DateTimeUtils import DateTimeUtils
 
 class Reservation:
+    """! This class represents a Reservation done on a flight or any other type of transportation.
+    This provides the user a seat number, their name, and the date of their flight.  
+    @author Marwa Hammami 
+    @since Feb 23, 2023
+    """
 
+
+    
     seatNumber = None
+   
     passengerName = None
+
     when = None
 
     def __init__(self, seatNumber,  passengerName) :
