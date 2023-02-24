486a376d7edb30e7aa76be6ca3949207521bc1a6
marwahamm04 <112507192+marwahamm04@users.noreply.github.com>
2023-02-24 00:14:30 -0500

    Update Reservation.py
    added documentation

--- src/thiagodnf/ars/commons/Reservation.py
+++ src/thiagodnf/ars/commons/Reservation.py
@@ -8,23 +8,37 @@ class Reservation:
     """
 
 
-    
+    """! This field provides the user their seatNumber"""
     seatNumber = None
-   
+    """! This provides the user their Name (passangerName)"""
     passengerName = None
-
+    """! This provides the user when their flight (or trip) is."""
     when = None
 
     def __init__(self, seatNumber,  passengerName) :
+        """! This method provides the user their seatNumber, their name (PassengerName), and when time of their flight.
+        @param seatnumber this is the users seat number
+        @param passengerName this is the passenger name
+        """
         self.seatNumber = seatNumber
         self.passengerName = passengerName
         self.when = DateTimeUtils.getNow()
 
     def  getSeatNumber(self) :
+        """! This method provides the user to get their seat number.
+        @return seatNumber
+        """
         return self.seatNumber
 
     def  getPassengerName(self) :
+        """! This method provides the user their name (passangerName).
+        @return passengerName
+        """
         return self.passengerName
 
     def  getWhenFormatted(self) :
+        """! This method provides the user  
+        @return the users dateTime of their flight. 
+        """
         return DateTimeUtils.format(self.when)
+
